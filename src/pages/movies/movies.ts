2623d447669460809a4601d9e1752d7c4bdd92d8
dany corbineau <danycor2@hotmail.fr>
2018-10-08 16:04:27 +0200

    Ajout d'une liste plus complète de filme

--- src/pages/movies/movies.ts
+++ src/pages/movies/movies.ts
@@ -15,14 +15,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MoviesPage {
   public movies_list=[
-    {nom:'a'},
-    {nom:'i'},
-    {nom:'h'},
-    {nom:'g'},
-    {nom:'f'},
-    {nom:'e'},
-    {nom:'d'},
-    {nom:'c'}
+    {Title:"ER",Year:"1994–2009",Rated:"TV-14",Released:"19 Sep 1994",Runtime:"44 min",Genre:"Drama, Romance"},
+    {Title:"Mee Pok Man",Year:"1996",Rated:"N/A",Released:"24 Nov 1996",Runtime:"105 min",Genre:"Drama, Romance"},
+    {Title:"Pierre",Year:"1985",Rated:"N/A",Released:"N/A",Runtime:"5 min",Genre:"Animation, Comedy, Short"},
+    {Title:"Coco",Year:"2017",Rated:"PG",Released:"22 Nov 2017",Runtime:"105 min",Genre:"Animation, Adventure, Comedy"}
   ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
